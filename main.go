b6eb82b8a92ffd7bc0e44a605ac147f73f0bf444
Oussama Atmani <borntolearn314@gmail.com>
2024-12-13 15:39:03 +0100

    parsed

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -9,38 +10,85 @@ import (
 
 type farm struct {
 	ants_number int
-	rooms       string
-	start       string
-	end         string
-	links       []string
+	rooms       map[string][]int
+	start       map[string][]int
+	end         map[string][]int
+	links       map[string][]string
 }
 
-func Read() {
-	bytes, err := os.ReadFile("test.txt")
+func main() {
+	var myFarm farm
+	myFarm.Read("test.txt")
+
+	fmt.Println("number of ants is : ", myFarm.ants_number)
+	fmt.Println("rooms are : ", myFarm.rooms)
+	fmt.Println("start is : ", myFarm.start)
+	fmt.Println("end is : ", myFarm.end)
+	fmt.Println("links are : ", myFarm.links)
+}
+
+func (myFarm *farm) Read(filename string) {
+	bytes, err := os.ReadFile(filename)
 	if err != nil {
 		log.Println("error reading", err)
 	}
 	content := strings.Split(string(bytes), "\n")
-	var ants, start, end, room, links farm
+
+	myFarm.rooms = make(map[string][]int)
+	myFarm.start = make(map[string][]int)
+	myFarm.end = make(map[string][]int)
+	myFarm.links = make(map[string][]string)
+
 	var st, en int
 	number, err := strconv.Atoi(content[0])
 	if err != nil {
 		log.Println("couldn't convert", err)
 	}
-	ants.ants_number = number
+	myFarm.ants_number = number
 
 	for index := range content {
 		if strings.TrimSpace(content[index]) == "##start" {
-			start.start = strings.TrimSpace(content[index+1])
 			st++
+			if index+1 <= len(content)-1 {
+				split := strings.Split(strings.TrimSpace(content[index+1]), " ")
+				x, err := strconv.Atoi(split[1])
+				y, err2 := strconv.Atoi(split[2])
+				if err == nil && err2 == nil {
+					myFarm.start[split[0]] = []int{x, y}
+				}
 
-		} else if strings.TrimSpace(content[index]) == "##end" {
-			end.end = strings.TrimSpace(content[index+1])
+			}
+
+		} else if strings.TrimSpace(content[index]) == "##end" && index+1 <= len(content)-1 {
 			en++
+			if index+1 <= len(content)-1 {
+				split := strings.Split(strings.TrimSpace(content[index+1]), " ")
+				x, err := strconv.Atoi(split[1])
+				y, err2 := strconv.Atoi(split[2])
+				if err == nil && err2 == nil {
+					myFarm.end[split[0]] = []int{x, y}
+				}
+
+			}
 		} else if strings.Contains(content[index], "-") {
-			links.links = strings.Split(strings.TrimSpace(content[index]), "-")
+			split := strings.Split(strings.TrimSpace(content[index]), "-")
+			if len(split) == 2 {
+				myFarm.links[split[0]] = append(myFarm.links[split[0]], split[1])
+			}
+		} else if strings.Count(content[index], " ") == 2 {
+			split := strings.Split(strings.TrimSpace(content[index]), " ")
+			if len(split) == 3 {
+				x, err := strconv.Atoi(split[1])
+				y, err2 := strconv.Atoi(split[2])
+				if err == nil || err2 == nil {
+					myFarm.rooms[split[0]] = []int{x, y}
+				}
+			}
 		} else if (strings.HasPrefix(strings.TrimSpace(content[index]), "#") || strings.HasPrefix(strings.TrimSpace(content[index]), "L")) && (strings.TrimSpace(content[index]) != "##start" && strings.TrimSpace(content[index]) != "##end") {
 			continue
 		}
+	}
+	if en != 1 || st != 1 {
+		log.Println("rooms setup is incorrect", err)
 	}
 }
